195243f4d7a84a5ffdcfda37c30de31f2cfee900
Nabanita Bag <ashley.makruz@gmail.com>
2021-03-20 13:19:31 +0530

    add max subarray sum

---
+++ Medium/KadanesAlgorithm.cpp
@@ -0,0 +1,18 @@
+//leetcode easy
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+
+        int maxEndingHere = nums[0],
+        maxSoFar = nums[0];
+        for(int i = 1 ; i < nums.size() ; i++ )
+        {
+            maxEndingHere = max(nums[i], maxEndingHere + nums[i]);
+            maxSoFar = max(maxSoFar, maxEndingHere);
+        }
+        return maxSoFar;
+    }
+};
+
+// O(n) time | O(1) space
