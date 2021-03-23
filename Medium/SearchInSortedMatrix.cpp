ba1ccadc2405b060dafaff26be68d29293a4c360
Nabanita Bag <ashley.makruz@gmail.com>
2021-03-24 01:57:43 +0530

    search in matrix

---
+++ Medium/SearchInSortedMatrix.cpp
@@ -0,0 +1,21 @@
+//leetcode medium
+
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+       int col = matrix[0].size()-1;
+       int row = 0;
+       while(row<matrix.size() && col>=0)
+       {
+           if(matrix[row][col] == target) return true;
+           if(matrix[row][col]<target)
+           {
+               row++;
+           }
+           else 
+           {
+               col--;
+           }
+       }
+       return false;
+   }
+
+   // O(n) time | O(1) space
