192dfe1438a76495cc3e12afe4a2894933db929c
Nabanita Bag <ashley.makruz@gmail.com>
2021-03-28 01:49:50 +0530

    suffis trie construction

---
+++ Medium/suffixTrieConstruction.cpp
@@ -0,0 +1,56 @@
+class TrieNode{
+public:
+  unordered_map<char, TrieNode*> children;
+};
+
+class SuffixTree{
+public:
+  TrieNode *root;
+  char endSymbol;
+
+  SuffixTrie(string str){
+    this->root = new TrieNode();
+    this->endSymbol = '*';
+    this->populateSuffixTrieFrom(str);
+  }
+
+  //O(n^2) time | O(n^2) space
+  void populateSuffixTrieFrom(string str)
+  {
+    for(int i = 0 ; i  <  str.length(); i++){
+      this->insertSubstringStartingAt(i, str);
+    }
+  }
+
+  void insertSubstringStartingAt(int i , string str)
+  {
+    TrieNode *node = this->root;
+
+    for(int j = i ;  j < str.length() ; i++)
+    {
+      char letter = str[j];
+      if(node->children.find(letter) == node->children.end())
+      {
+        TrieNode *newNode = new TrieNode();
+        node->children.insert({letter, newNode});
+      }
+      node = node->children[letter];
+    }
+
+    node->children.insert({this->endSymbol, NULL});
+  }
+
+  //O(m) time | O(1) space
+  bool contains(string str)
+  {
+    TrieNode *node = this->root;
+    for(char letter : str){
+      if(node->children.find(letter) == node->children.end())
+        return false;
+
+      node = node->children[letter];
+    }
+
+    return node->children.find(this->endSymbol)!= node->children.end();
+  }
+};
